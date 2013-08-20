2f44c085db0d18804b4f8f7ba171c47a617a1df8
EvgenyKarkan <evgenykarkan@gmail.com>
2013-08-20 23:23:16 +0300

    Minor fixes

--- EKAlgorithms/EKAlgorithms/main.m
+++ EKAlgorithms/EKAlgorithms/main.m
@@ -16,22 +16,26 @@ int main(int argc, const char *argv[])
 	@autoreleasepool {
 		
 			//Linear search
-		NSLog(@"Linear search result: %li", (long)[EKSearch indexOfObjectViaLinearSearchForObject:@42 inArray:@[@45, @26, @125, @42]]);
-		
-			//Bubble sort
-		NSLog(@"Bubble sorted result: %@", [EKSort bubbleSortedArrayWithUnsortedArray:[NSMutableArray arrayWithObjects:@4.5, @17.4, @12, @27, @6, @1, @0.5, nil]]);
+		NSLog(@"Linear search result: %li", (long)[EKSearch indexOfObjectViaLinearSearchForObject:@42 inArray:@[@6, @9, @12, @13, @14, @29, @42]]);
 		
 			//Binary search
 		NSLog(@"Binary search result: %li", (long)[EKSearch inedxOfObjectViaBinarySearchForObject:@42 inSortedArray:@[@6, @9, @12, @13, @14, @29, @42]]);
 		
-			//Max element of array
+			//Init array with 5 random elements
 		NSMutableArray *array = [NSMutableArray array];
 		for (int i = 0; i < 5; i++) {
 			[array addObject:[NSNumber numberWithInt:arc4random() % 20]];
 		}
+		
+			//Max element of array
 		NSLog(@"Max element of created array %@ is equals to %@ and stored at index %li",[array debugDescription],
-																						 [array objectAtIndex:(long)[EKArray indexOfMaximumElementInArray:array]],
-																						 (long)[EKArray indexOfMaximumElementInArray:array]);
+			  [array objectAtIndex:(long)[EKArray indexOfMaximumElementInArray:array]],
+			  (long)[EKArray indexOfMaximumElementInArray:array]);
+
+			//Bubble sort
+		NSLog(@"Bubble sorted array is: %@", [EKSort bubbleSortedArrayWithUnsortedArray:array]);
+		
+		
 	}
 	return 0;
 }
