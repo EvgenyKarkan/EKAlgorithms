b8d58517fec733dd58ff5e55640ed66f24c281dc
Stanislaw Pankevich <s.pankevich@gmail.com>
2013-12-20 10:44:18 +0400

    Prefer machine way of doing even/odd checks

---
+++ EKAlgorithmsSpecs/NSNumber.m
@@ -0,0 +1,28 @@
+#import <Kiwi/Kiwi.h>
+
+#import "NSNumber+EKStuff.h"
+
+SPEC_BEGIN(NSNumber_Specs)
+
+describe(@"NSNumber-based algorithms", ^{
+    describe(@"isEven", ^{
+        it(@"should return YES if number is even and NO otherwise", ^{
+            int N = 100;
+
+            for (int i = 0; i < N; i++) {
+                BOOL isEvenMathematically = [@(i % 2) boolValue] == NO;
+
+                [[theValue([@(i) isEven]) should] equal:@(isEvenMathematically)];
+
+                if ([@(i) isEven]) {
+                    [[theValue([@(i + 1) isEven]) should] beNo];
+                } else {
+                    [[theValue([@(i + 1) isEven]) should] beYes];
+                }
+            }
+        });
+    });
+    
+});
+
+SPEC_END
