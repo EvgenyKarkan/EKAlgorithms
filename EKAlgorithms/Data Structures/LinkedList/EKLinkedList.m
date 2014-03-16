cbd7d2e75b6a79db42eebef065f00d699ff35551
Yifei Zhou <zhouyifei@me.com>
2014-03-16 14:08:46 +0800

    Fix the problem that self.tail is nil when call AddToBack method after a linked list is initialized.

--- EKAlgorithms/Data Structures/LinkedList/EKLinkedList.m
+++ EKAlgorithms/Data Structures/LinkedList/EKLinkedList.m
@@ -34,6 +34,13 @@
 {
     EKNode *node = [[EKNode alloc] initWithObject:value];
     
+    if (self.tail == nil) {
+        EKNode *lastNode = self.head;
+        for (NSUInteger i = 1; i < self.count; i++) {
+            lastNode = lastNode.next;
+        }
+        self.tail = lastNode;
+    }
     node.previous = self.tail;
     self.tail.next = node;
     self.tail = node;
