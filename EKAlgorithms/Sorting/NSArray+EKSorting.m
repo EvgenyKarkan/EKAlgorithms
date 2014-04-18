7ea99c8bed694cc622ed720372f7335cade57cd5
Evgeny Karkan <evgenykarkan@gmail.com>
2014-04-18 23:57:40 +0300

    Minor code formate.

--- EKAlgorithms/Sorting/NSArray+EKSorting.m
+++ EKAlgorithms/Sorting/NSArray+EKSorting.m
@@ -206,8 +206,8 @@
 {
     NSMutableArray *theArray = [self copy];
     int max = [[theArray valueForKeyPath:@"@max.intValue"] intValue];
-    int numberOfSteps = (log(max) / log(base))+1;
-    for (int i = 0; i< numberOfSteps; i++) {
+    int numberOfSteps = (log(max) / log(base)) + 1;
+    for (int i = 0; i < numberOfSteps; i++) {
         NSMutableArray *radixTable = [NSMutableArray makeRadixTableForArray:theArray forBase:base forDigit:i];
         theArray = [NSMutableArray makeArrayFromRadixTable:radixTable];
     }
@@ -217,7 +217,7 @@
 + (NSMutableArray *)makeArrayFromRadixTable:(NSMutableArray *)radixTable
 {
     NSMutableArray *theArray = [NSMutableArray new];
-    for (RadixNode *bucketNode in radixTable){
+    for (RadixNode *bucketNode in radixTable) {
         RadixNode *bucket = bucketNode.next;
         while (bucket) {
             [theArray addObject:@(bucket.data)];
@@ -231,12 +231,11 @@
 {
     NSMutableArray *radixTable = [self getTableOfEmptyBucketsForSize:base];
     for (int i = 0; i < theArray.count; i++) {
-        NSInteger value =[[theArray objectAtIndex:i] integerValue];
+        NSInteger value = [[theArray objectAtIndex:i] integerValue];
         NSInteger radixIndex = [self getExaminedNumber:value withBase:base atDigit:digit];
         RadixNode *current = (RadixNode *)[radixTable objectAtIndex:radixIndex];
         if (current.next) {
             while (current.next) {
-                
                 current = [current next];
             }
         }
@@ -261,7 +260,7 @@
 + (NSInteger)getExaminedNumber:(NSInteger)number withBase:(NSInteger)base atDigit:(NSInteger)digit
 {
     NSInteger divisor = (digit == 0) ? 1 : (pow(base, digit));
-    return (number/divisor) % base;
+    return (number / divisor) % base;
 }
 
 
@@ -292,7 +291,6 @@
 
     return self;
 }
-
 
 #pragma mark - Heap sort
 
