08677f8da442a13eca9584190a9a6e2f3a3c54bd
Ahan Malhotra <ahan.tm@randomwidgets.com>
2015-01-24 21:42:43 -0500

    FIX prime number method to include that 0 is NOT a prime number. Also added a Prime Number Test Spec.

--- Specs/NSNumber.m
+++ Specs/NSNumber.m
@@ -22,6 +22,21 @@ describe(@"NSNumber-based algorithms", ^{
             }
         });
     });
+    
+    describe(@"isPrime", ^{
+        it(@"should return YES if number is prime and NO otherwise", ^{
+            int N = 100;
+            NSArray *primesUnderN = @[ @2, @3, @5, @7, @11, @13, @17, @19, @23, @29, @31, @37, @41, @43, @47, @53, @59, @61, @67, @71, @73, @79, @83, @89, @97];
+            
+            for (int i = 0; i < N; i++) {
+                if ([primesUnderN containsObject:@(i)]) {
+                    [[theValue([@(i) isPrime]) should] beYes];
+                } else {
+                    [[theValue([@(i) isPrime]) should] beNo];
+                }
+            }
+        });
+    });
 });
 
 SPEC_END
